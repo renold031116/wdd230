0c85dbba9231e7237755a9c5c7902ca993421a6b
renold03116 <renold031116@gmail.com>
2024-05-09 22:38:48 -0600

    push

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -4,3 +4,11 @@ document.addEventListener("DOMContentLoaded", () => {
 
     document.getElementById("lastModified").innerHTML = "Last Modified: " + document.lastModified;
    });
+
+   const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('open');
+	hamButton.classList.toggle('open');
+});
